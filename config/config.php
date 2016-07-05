fc72127f1be8607dfe74d0489fdf1caf9500740b
fwgithub <2682771419@qq.com>
2016-07-05 17:02:24 +0800

    second commit

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername  = 'localhost';
+$dbname      = 'WeLive';
+$dbusername  = 'root';
+$dbpassword  = 'root';
+
+define('WELIVE', true);
+define('TABLE_PREFIX', 'welive_');
+define('COOKIE_KEY', '3omIYV8UxN8P');
+define('WEBSITE_KEY', 'BOYLiG2H97ku');
+define('BASEPATH', dirname(dirname(__FILE__)).'/');
+
+?>
---
+++ config/settings.php
@@ -0,0 +1,24 @@
+<?php
+
+$_CFG = array();
+
+$_CFG['cActived'] = "1";
+$_CFG['cUpdate'] = "3";
+$_CFG['cAutoOffline'] = "10";       //10分钟断开
+$_CFG['cLang'] = "Chinese";
+$_CFG['cTimezone'] = "+8";
+$_CFG['cDateFormat'] = "Y-m-d";
+$_CFG['cTitle'] = "在线客服系统";
+$_CFG['cTitle_en'] = "WeLive Online Support";
+$_CFG['cWelcome'] = "欢迎您登录客服系统, 请咨询, 谢谢!";
+$_CFG['cWelcome_en'] = "Welcome! please post your question...";
+$_CFG['cAppVersion'] = '3.5.0';
+$_CFG['cAppName'] = "V2VMaXZl";
+$_CFG['cAppCopyrighURL'] = "aHR0cDovL3d3dy53ZWVudGVjaC5jb20v";
+$_CFG['cKillRobotCode'] = 'cd18d26a6e0fe0a8c386e3d9496547f7';
+$_CFG['cDeleteHistory'] = "0";
+$_CFG['cBannedips'] = "";
+$_CFG['cBadwords'] = "";
+$_CFG['cPanalHeight'] = "200";
+
+?>
